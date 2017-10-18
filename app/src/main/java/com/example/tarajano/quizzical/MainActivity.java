eb8f8240a6097d6b530a4471086ec785c9cdf62e
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-18 13:28:20 -0400

    SUMMARY: Ads reset button. REMARKS: TODO:
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:

--- app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
+++ app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
@@ -11,8 +11,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonTrue, buttonFalse;
+    private Button buttonTrue, buttonFalse, buttonReset;
     private TextView questionText, answerText;
+    private View mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
         // Creating refs for buttons
         buttonTrue = (Button) findViewById(R.id.buttontTrue);
         buttonFalse = (Button) findViewById(R.id.buttontFalse);
+        buttonReset = (Button) findViewById(R.id.buttontReset);
 
+        // Refs to text
         questionText = (TextView) findViewById(R.id.questionText);
         questionText.setText("Great workout this morning right?");
-
         answerText = (TextView) findViewById(R.id.anserwText);
+
+        // Refs to activity
+        mainActivity = (View) findViewById(R.id.mainActivity);
 
         // Setting actions for buttons
         buttonTrue.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.e("bootcamp", "True button clicked");
                 answerText.setText("Sure it was!");
+                mainActivity.setBackgroundColor(Color.parseColor("#47d147"));
             }
         });
 
@@ -41,10 +47,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.e("bootcamp", "False button clicked");
-                answerText.setText("Ohh! You are in great shape.");
+                answerText.setText("Ups! I disagree.");
+                mainActivity.setBackgroundColor(Color.parseColor("#ff471a"));
             }
         });
 
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("bootcamp", "Reset button clicked");
+                answerText.setText("");
+                mainActivity.setBackgroundColor(Color.parseColor("#cce6ff"));
+            }
+        });
 
     }
 
