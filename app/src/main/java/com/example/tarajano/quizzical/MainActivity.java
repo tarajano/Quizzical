855b1b928c9156dab6fb4585119a967d4e58ca3b
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-18 11:17:26 -0400

    SUMMARY: Adds objects for True in False buttons. REMARKS: TODO:
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:

--- app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
+++ app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tarajano.quizzical;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Creating refs for buttons
+        Button buttonTrue = (Button) findViewById(R.id.buttontTrue);
+        Button buttonFalse = (Button) findViewById(R.id.buttontFalse);
+
+        
     }
 
 }
