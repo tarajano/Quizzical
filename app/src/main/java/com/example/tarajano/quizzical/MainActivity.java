f55c9ee4a621e26cd0420347447240cd35c08d0e
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-18 11:31:48 -0400

    SUMMARY: Ads setOnClickListener() and Log.e() for True and False buttons.
    REMARKS:
    TODO:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:

--- app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
+++ app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.tarajano.quizzical;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonTrue, buttonFalse;
+    private TextView questionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Creating refs for buttons
-        Button buttonTrue = (Button) findViewById(R.id.buttontTrue);
-        Button buttonFalse = (Button) findViewById(R.id.buttontFalse);
+        buttonTrue = (Button) findViewById(R.id.buttontTrue);
+        buttonFalse = (Button) findViewById(R.id.buttontFalse);
 
-        
+        questionText = (TextView) findViewById(R.id.questionText);
+        questionText.setText("Hello from code.");
+
+        // Setting actions for buttons
+        buttonTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("bootcamp", "True button clicked");
+            }
+        });
+
+        buttonFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("bootcamp", "False button clicked");
+            }
+        });
     }
 
 }
