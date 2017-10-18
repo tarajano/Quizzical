c351e065cb520c84b9cc47d2b9d0ef09a238456e
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-18 11:45:13 -0400

    SUMMARY: Ads a TextView for answerText object and set onClick actions for buttons. Once a button (True/False) is clicked a respnse is shown in set in answerText  and displayed in TextView. REMARKS: TODO:
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:
    
    FILE:
      ADDS:
      MODIFIES:
      REMOVES:

--- app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
+++ app/src/main/java/com/example/tarajano/quizzical/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.tarajano.quizzical;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonTrue, buttonFalse;
-    private TextView questionText;
+    private TextView questionText, answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +24,16 @@ public class MainActivity extends AppCompatActivity {
         buttonFalse = (Button) findViewById(R.id.buttontFalse);
 
         questionText = (TextView) findViewById(R.id.questionText);
-        questionText.setText("Hello from code.");
+        questionText.setText("Great workout this morning right?");
+
+        answerText = (TextView) findViewById(R.id.anserwText);
 
         // Setting actions for buttons
         buttonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.e("bootcamp", "True button clicked");
+                answerText.setText("Sure it was!");
             }
         });
 
@@ -36,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.e("bootcamp", "False button clicked");
+                answerText.setText("Ohh! You are in great shape.");
             }
         });
+
+
     }
 
 }
